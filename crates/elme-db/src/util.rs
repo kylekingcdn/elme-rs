95aebe96c2bc3db03499c16f958552b8d10a4b2c
Kyle King <git@kylek.ing>
2026-08-14 11:50:30 -0700

    feat(elme-db): add query chaining utils

--- crates/elme-db/src/util.rs
+++ crates/elme-db/src/util.rs
@@ -1,3 +1,9 @@
+use sqlx::{
+    postgres::{PgArguments, PgPool, Postgres},
+    query::Query
+};
+use tracing::instrument;
+
 #[derive(Debug, Clone)]
 pub struct PartialVec<T> {
     pub inner: Vec<T>,
@@ -31,6 +37,56 @@ impl SortOrder {
         match self {
             Self::Asc => Self::Desc,
             Self::Desc => Self::Asc,
+        }
+    }
+}
+
+/// Executes each query sequentially, aborting on the first error
+///
+/// Primary use-case is providing the logic for fail-fast tx rollback
+///
+/// # Errors
+///
+/// Passes through the first encountered [`sqlx::Error`], if any
+#[instrument(skip_all, err, fields(query_count=queries.len()))]
+pub async fn execute_queries(
+    db: &PgPool,
+    queries: Vec<Query<'_, Postgres, PgArguments>>,
+) -> Result<(), sqlx::Error> {
+    for query in queries {
+        query.execute(db).await?;
+    }
+    Ok(())
+}
+
+/// Executes each query sequentially, aborting on the first errors.
+///
+/// Wraps the execution chain in a tx. All queries in the chain are rolled back upon failure.
+///
+/// # Errors
+///
+/// Passes through the first encountered [`sqlx::Error`], if any
+#[instrument(skip_all, err, fields(query_count=queries.len()))]
+pub async fn execute_queries_with_tx(
+    db: &PgPool,
+    queries: Vec<Query<'_, Postgres, PgArguments>>,
+) -> Result<(), sqlx::Error> {
+    // ! TODO: add debug chain queries feature flag for opt-in logging of specific failure sql
+    // let mut queries_sql = Vec::new();
+    // for query in &queries {
+    //     queries_sql.push(query.sql());
+    // }
+    let tx = db.begin().await?;
+    match execute_queries(db, queries).await {
+        Ok(()) => {
+            tx.commit().await?;
+            Ok(())
+        },
+        Err(error) => {
+            tx.rollback().await?;
+            tracing::error!(?error, "Failed to execute query chain, queries were rolled back");
+            // tracing::error!("Queries: {queries_sql:#?}");
+            Err(error)?
         }
     }
 }
