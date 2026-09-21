fdeadce9c474c87da0d69a24de5a8822729751dc
Kyle King <git@kylek.ing>
2026-09-20 18:50:02 -0700

    doc: streamline example

--- crates/elme-shutdown/examples/basic/main.rs
+++ crates/elme-shutdown/examples/basic/main.rs
@@ -1,30 +1,33 @@
 mod worker;
 use worker::WorkerManager;
 
-use elme_shutdown::{Command, ShutdownConfig, ShutdownManager, StopCommand};
-use std::time::Duration;
+use elme_shutdown::{ShutdownConfig, ShutdownManager};
 use std::process::ExitCode;
+use std::time::Duration;
 use tracing_subscriber::{EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<ExitCode> {
-    // - begin one-time init
+
+    // ---- begin one-time init
+
     color_eyre::install()?;
 
-    // setup elme-shutdown
+    // - setup elme-shutdown
     let shutdown_config = ShutdownConfig::builder()
         .timeout(Duration::from_secs(15))
         .build();
     let shutdown_mgr = ShutdownManager::init(shutdown_config);
+
+    // - setup tracing
     let filter_layer = EnvFilter::try_from_default_env()
-        .or_else(|_| EnvFilter::try_new("info"))
-        .unwrap();
+        .or_else(|_| EnvFilter::try_new("info"))?;
     let fmt_layer = tracing_subscriber::fmt::layer()
         .with_ansi(true)
         .with_ansi_sanitization(false);
 
-    // prevent broken tracing logs w/ progress
-    #[cfg(feature = "progress-writer")]
+    // prevent broken tracing logs w/ elme-shutdown progress bars
+    #[cfg(feature = "progress-writer")] // gated due to tracing-subscriber dependancy
     let fmt_layer = fmt_layer.with_writer(shutdown_mgr.progress_writer());
 
     tracing_subscriber::registry()
@@ -32,36 +35,27 @@ async fn main() -> color_eyre::Result<ExitCode> {
         .with(fmt_layer)
         .init();
 
-    // init + run loop
-    while shutdown_mgr.app_should_start() {
+    // ---- end one-time init
 
-        // – init app - //
-        tracing::info!("Handling app startup...");
+    // - init + run loop
+    //   returns control upon stop command teardown completion
+    while shutdown_mgr.app_should_start() { // provides support for live-reload
+
+        // – init app
+        //   any init that is not 'one-time' should happen here
         shutdown_mgr.inform_starting().unwrap();
+        let workers = WorkerManager::new(shutdown_mgr.register_task("Worker manager")?);
 
-        let workers = WorkerManager::new(shutdown_mgr.register_task("Worker manager").unwrap());
-        std::thread::sleep(Duration::from_millis(500)); // simulate longer startup with 0,5s delay
-
-        // – run app - //
-        shutdown_mgr.inform_started().unwrap();
-
-        // the worker manager's run loop ends upon teardown, so thats all we need to wait on
-        workers.run().await?;
-
-        // - teardown app -- //
-        tracing::info!("Teardown has started... waiting for graceful stop");
-        // now wait for tasks to end gracefully
-        shutdown_mgr.wait_for_teardown_done().await;
-        tracing::info!("Tasks gracefully stopped\n\n----\n");
+        // – run app
+        shutdown_mgr.inform_started()?;
+        workers.run().await?; // the worker manager's run loop ends once teardown
+                              // starts, so thats all we need to wait on
+        // - teardown
+        shutdown_mgr.wait_for_teardown_done().await; // now wait for tasks to end gracefully
     }
 
-    // - this will only be reached at the end of teardown
-    //   if we are stopping (as opposed to reloading)
-    let code = match shutdown_mgr.issued_command() {
-        Some(Command::Stop(StopCommand { exit_code })) => exit_code,
-        _ => -1
-    };
+    // retrieve exit code provided by user calls to `stop()`
+    let exit_code = shutdown_mgr.exit_code().unwrap_or(0);
 
-    #[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
-    Ok(ExitCode::from(code as u8))
+    Ok(exit_code.into())
 }
